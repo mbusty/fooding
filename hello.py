7d0da6cc313295ec965378bdc9c235b47f1a8852
Miguel <m.bustillo96@gmail.com>
2022-06-16 14:20:38 -0400

    bootstrapping

--- hello.py
+++ hello.py
@@ -1,24 +1,28 @@
+# TODO: Get rid of 'user'
+
 from flask import Flask, render_template
 
 # Create a Flask Instance
 app = Flask(__name__)
 
-# Create a route decorator 
+# Index page, playing around with Jinja
 @app.route('/')
 def index():
     first_name = 'Miguel'
     return render_template('index.html', 
         first_name=first_name)
 
+# Alternate page to use templates
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', user_name=name)
 
-# Custom error pages
+# 404 Error page
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
+# 500 Error page
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
