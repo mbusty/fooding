bf76e6fec8724c19dffa9aa47f4cfb2995379526
Miguel <m.bustillo96@gmail.com>
2022-06-16 14:56:06 -0400

    added basic user form

--- hello.py
+++ hello.py
@@ -1,9 +1,20 @@
 # TODO: Get rid of 'user'
 
+from ast import Sub
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
 
 # Index page, playing around with Jinja
 @app.route('/')
@@ -26,6 +37,21 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template('name.html',
+        name = name,
+        form = form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
