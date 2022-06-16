a5c370bb3633eacc72d009d812b0b7c786b622e3
Miguel <m.bustillo96@gmail.com>
2022-06-16 15:30:32 -0400

    added alerts using flash

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 # TODO: Get rid of 'user'
 
-from ast import Sub
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -46,6 +45,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash('Form Submitted Successfully!')
 
     return render_template('name.html',
         name = name,
